ecf751088265c77205393f10c9835f06c26ccdd9
Cyrille Savelief <csavelief@mncc.fr>
2019-12-29 15:50:42 +0100

    Cleanup & improve tests for If() function with a mixed int/double test.

--- test/com/computablefacts/nona/functions/controlflowoperators/IfTest.java
+++ test/com/computablefacts/nona/functions/controlflowoperators/IfTest.java
@@ -13,7 +13,40 @@ import com.computablefacts.nona.types.BoxedType;
 public class IfTest {
 
   @Test
-  public void testIf() {
+  public void testIfWithIntegers() {
+
+    Map<String, Function> functions = new HashMap<>();
+    functions.put("IF", new If());
+    functions.put("GTE", new GreaterThanOrEqual());
+
+    Function fn = new Function("IF(GTE(1, 2), 1, 2)");
+    Assert.assertEquals(BoxedType.create(2), fn.evaluate(functions));
+  }
+
+  @Test
+  public void testIfWithDoubles() {
+
+    Map<String, Function> functions = new HashMap<>();
+    functions.put("IF", new If());
+    functions.put("GTE", new GreaterThanOrEqual());
+
+    Function fn = new Function("IF(GTE(1.1, 2.2), 1.1, 2.2)");
+    Assert.assertEquals(BoxedType.create(2.2), fn.evaluate(functions));
+  }
+
+  @Test
+  public void testIfWithIntegersAndDoubles() {
+
+    Map<String, Function> functions = new HashMap<>();
+    functions.put("IF", new If());
+    functions.put("GTE", new GreaterThanOrEqual());
+
+    Function fn = new Function("IF(GTE(1, 1.0), 1, 1.0)");
+    Assert.assertEquals(BoxedType.create(1), fn.evaluate(functions));
+  }
+
+  @Test
+  public void testIfWithStrings() {
 
     Map<String, Function> functions = new HashMap<>();
     functions.put("IF", new If());
