7c84f64cd555ed000f508a1553a11cf0168da3ae
Cyrille Savelief <csavelief@mncc.fr>
2020-05-07 00:10:47 +0200

    Backport DICTIONARY extraction.

---
+++ src/com/computablefacts/nona/functions/stringoperators/MatchDictionary.java
@@ -0,0 +1,85 @@
+package com.computablefacts.nona.functions.stringoperators;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import javax.validation.constraints.NotNull;
+
+import org.ahocorasick.trie.Emit;
+import org.ahocorasick.trie.Trie;
+
+import com.computablefacts.nona.Function;
+import com.computablefacts.nona.types.BoxedType;
+import com.computablefacts.nona.types.Span;
+import com.computablefacts.nona.types.SpanSequence;
+import com.google.common.base.Preconditions;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.errorprone.annotations.CheckReturnValue;
+import com.google.errorprone.annotations.Var;
+
+@CheckReturnValue
+public class MatchDictionary extends Function {
+
+  private static final LoadingCache<String, Trie> cache_ =
+      CacheBuilder.newBuilder().recordStats().maximumSize(10).expireAfterWrite(30, TimeUnit.MINUTES)
+          .build(new CacheLoader<String, Trie>() {
+
+            @Override
+            public Trie load(@NotNull String file) {
+
+              // See https://github.com/robert-bor/aho-corasick for details
+              Trie.TrieBuilder builder =
+                  Trie.builder().ignoreCase().ignoreOverlaps().onlyWholeWords();
+
+              try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+
+                @Var
+                String keyword;
+
+                while ((keyword = br.readLine()) != null) {
+                  builder.addKeyword(keyword.trim());
+                }
+              } catch (IOException e) {
+                // TODO
+              }
+              return builder.build();
+            }
+          });
+
+  public MatchDictionary() {
+    super("MATCHDICTIONARY", true);
+  }
+
+  @Override
+  protected BoxedType evaluate(List<BoxedType> parameters) {
+
+    Preconditions.checkArgument(parameters.size() == 2,
+        "MATCHDICTIONARY takes exactly four parameters.");
+    Preconditions.checkArgument(parameters.get(0).isString(), "%s should be a string",
+        parameters.get(0));
+    Preconditions.checkArgument(parameters.get(1).isString(), "%s should be a string",
+        parameters.get(1));
+
+    String file = parameters.get(0).asString();
+    String text = parameters.get(1).asString();
+
+    Preconditions.checkArgument(new File(file).exists(), "%s does not exist", file);
+
+    Trie trie = cache_.getUnchecked(file);
+    Collection<Emit> emits = trie.parseText(text);
+    SpanSequence sequence = new SpanSequence();
+
+    for (Emit emit : emits) {
+      Span span = new Span(text, emit.getStart(), emit.getEnd() + 1);
+      sequence.add(span);
+    }
+    return BoxedType.create(sequence);
+  }
+}
---
+++ test/com/computablefacts/nona/functions/stringoperators/MatchDictionaryTest.java
@@ -0,0 +1,68 @@
+package com.computablefacts.nona.functions.stringoperators;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.computablefacts.nona.Function;
+import com.computablefacts.nona.types.Span;
+import com.computablefacts.nona.types.SpanSequence;
+import com.google.common.collect.Lists;
+
+public class MatchDictionaryTest {
+
+  @Test
+  public void testExactlyOneMatch() throws IOException {
+
+    // Create dictionary
+    List<String> keywords = Lists.newArrayList("sugar");
+    Path file = Files.createTempFile("dico-", ".txt");
+    Files.write(file, keywords);
+
+    // Execute function
+    Map<String, Function> functions = new HashMap<>();
+    functions.put("DICO", new MatchDictionary());
+
+    Function fn = new Function("DICO(" + Function.wrap(file.toString()) + ", "
+        + Function.wrap("sugarcane sugar canesugar") + ")");
+    List<Span> spans = ((SpanSequence) fn.evaluate(functions).value()).sequence();
+
+    Assert.assertEquals(1, spans.size());
+    Assert.assertEquals("sugar", spans.get(0).text());
+    Assert.assertEquals(10, spans.get(0).begin());
+    Assert.assertEquals(15, spans.get(0).end());
+  }
+
+  @Test
+  public void testMoreThanOneMatch() throws IOException {
+
+    // Create dictionary
+    List<String> keywords = Lists.newArrayList("great question", "forty-two", "deep thought");
+    Path file = Files.createTempFile("dico-", ".txt");
+    Files.write(file, keywords);
+
+    // Execute function
+    Map<String, Function> functions = new HashMap<>();
+    functions.put("DICO", new MatchDictionary());
+
+    Function fn = new Function("DICO(" + Function.wrap(file.toString()) + ", " + Function.wrap(
+        "The Answer to the Great Question... Of Life,\nthe Universe and Everything... Is... Forty-two,' said\nDeep Thought, with infinite majesty and calm.")
+        + ")");
+    List<Span> spans = ((SpanSequence) fn.evaluate(functions).value()).sequence();
+
+    Assert.assertEquals(3, spans.size());
+    Assert.assertTrue(
+        spans.stream().map(Span::text).collect(Collectors.toSet()).contains("Great Question"));
+    Assert.assertTrue(
+        spans.stream().map(Span::text).collect(Collectors.toSet()).contains("Forty-two"));
+    Assert.assertTrue(
+        spans.stream().map(Span::text).collect(Collectors.toSet()).contains("Deep Thought"));
+  }
+}
