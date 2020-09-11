743c9cf6dc24ccc40f4890a43d834d5fc33d3089
Cyrille Savelief <csavelief@mncc.fr>
2020-09-11 20:42:19 +0200

    Backport tests for mixed bags of bigrams and trigrams.

--- test/com/computablefacts/nona/helpers/IBagOfNGramsTest.java
+++ test/com/computablefacts/nona/helpers/IBagOfNGramsTest.java
@@ -61,6 +61,13 @@ public class IBagOfNGramsTest {
     IBagOfNGrams bag = bagOfNGrams();
 
     Set<List<String>> ngrams = new HashSet<>();
+
+    ngrams.add(Lists.newArrayList("a", "b"));
+    ngrams.add(Lists.newArrayList("a", "d"));
+    ngrams.add(Lists.newArrayList("c", "d"));
+    ngrams.add(Lists.newArrayList("c", "e"));
+    ngrams.add(Lists.newArrayList("f", "g"));
+
     ngrams.add(Lists.newArrayList("a", "b", "c"));
     ngrams.add(Lists.newArrayList("a", "b", "d"));
     ngrams.add(Lists.newArrayList("c", "d", "e"));
@@ -74,7 +81,7 @@ public class IBagOfNGramsTest {
 
     IBagOfNGrams bag = bagOfNGrams();
 
-    Assert.assertEquals(6, bag.numberOfNGrams());
+    Assert.assertEquals(13, bag.numberOfNGrams());
   }
 
   @Test
@@ -82,7 +89,7 @@ public class IBagOfNGramsTest {
 
     IBagOfNGrams bag = bagOfNGrams();
 
-    Assert.assertEquals(4, bag.numberOfDistinctNGrams());
+    Assert.assertEquals(9, bag.numberOfDistinctNGrams());
   }
 
   @Test
@@ -90,9 +97,11 @@ public class IBagOfNGramsTest {
 
     IBagOfNGrams bag = bagOfNGrams();
 
-    Assert.assertEquals(0, bag.frequency("a"));
-    Assert.assertEquals(0, bag.frequency("a", "b"));
-    Assert.assertEquals(0, bag.frequency("a", "b", "e"));
+    Assert.assertEquals(1, bag.frequency("a", "b"));
+    Assert.assertEquals(1, bag.frequency("a", "d"));
+    Assert.assertEquals(1, bag.frequency("c", "d"));
+    Assert.assertEquals(3, bag.frequency("c", "e"));
+    Assert.assertEquals(1, bag.frequency("f", "g"));
 
     Assert.assertEquals(1, bag.frequency("a", "b", "c"));
     Assert.assertEquals(1, bag.frequency("a", "b", "d"));
@@ -104,6 +113,12 @@ public class IBagOfNGramsTest {
   public void testNormalizedFrequency() {
 
     IBagOfNGrams bag = bagOfNGrams();
+
+    Assert.assertEquals(1.0 / 7.0, bag.normalizedFrequency("a", "b"), 0.00000001);
+    Assert.assertEquals(1.0 / 7.0, bag.normalizedFrequency("a", "d"), 0.00000001);
+    Assert.assertEquals(1.0 / 7.0, bag.normalizedFrequency("c", "d"), 0.00000001);
+    Assert.assertEquals(3.0 / 7.0, bag.normalizedFrequency("c", "e"), 0.00000001);
+    Assert.assertEquals(1.0 / 7.0, bag.normalizedFrequency("f", "g"), 0.00000001);
 
     Assert.assertEquals(1.0 / 6.0, bag.normalizedFrequency("a", "b", "c"), 0.00000001);
     Assert.assertEquals(1.0 / 6.0, bag.normalizedFrequency("a", "b", "d"), 0.00000001);
@@ -120,21 +135,42 @@ public class IBagOfNGramsTest {
     Multiset<List<String>> ngrams = bag1.mergeWith(bag2);
 
     // Ensure bag1 has not been modified
-    Assert.assertEquals(6, bag1.numberOfNGrams());
+    Assert.assertEquals(13, bag1.numberOfNGrams());
+
+    Assert.assertEquals(1, bag1.frequency("a", "b"));
+    Assert.assertEquals(1, bag1.frequency("a", "d"));
+    Assert.assertEquals(1, bag1.frequency("c", "d"));
+    Assert.assertEquals(3, bag1.frequency("c", "e"));
+    Assert.assertEquals(1, bag1.frequency("f", "g"));
+
     Assert.assertEquals(1, bag1.frequency("a", "b", "c"));
     Assert.assertEquals(1, bag1.frequency("a", "b", "d"));
     Assert.assertEquals(3, bag1.frequency("c", "d", "e"));
     Assert.assertEquals(1, bag1.frequency("c", "d", "d"));
 
     // Ensure bag2 has not been modified
-    Assert.assertEquals(6, bag2.numberOfNGrams());
+    Assert.assertEquals(13, bag2.numberOfNGrams());
+
+    Assert.assertEquals(1, bag2.frequency("a", "b"));
+    Assert.assertEquals(1, bag2.frequency("a", "d"));
+    Assert.assertEquals(1, bag2.frequency("c", "d"));
+    Assert.assertEquals(3, bag2.frequency("c", "e"));
+    Assert.assertEquals(1, bag2.frequency("f", "g"));
+
     Assert.assertEquals(1, bag2.frequency("a", "b", "c"));
     Assert.assertEquals(1, bag2.frequency("a", "b", "d"));
     Assert.assertEquals(3, bag2.frequency("c", "d", "e"));
     Assert.assertEquals(1, bag2.frequency("c", "d", "d"));
 
     // Ensure merge is ok
-    Assert.assertEquals(12, ngrams.size());
+    Assert.assertEquals(26, ngrams.size());
+
+    Assert.assertEquals(2, ngrams.count(Lists.newArrayList("a", "b")));
+    Assert.assertEquals(2, ngrams.count(Lists.newArrayList("a", "d")));
+    Assert.assertEquals(2, ngrams.count(Lists.newArrayList("c", "d")));
+    Assert.assertEquals(6, ngrams.count(Lists.newArrayList("c", "e")));
+    Assert.assertEquals(2, ngrams.count(Lists.newArrayList("f", "g")));
+
     Assert.assertEquals(2, ngrams.count(Lists.newArrayList("a", "b", "c")));
     Assert.assertEquals(2, ngrams.count(Lists.newArrayList("a", "b", "d")));
     Assert.assertEquals(6, ngrams.count(Lists.newArrayList("c", "d", "e")));
@@ -142,7 +178,54 @@ public class IBagOfNGramsTest {
   }
 
   @Test
-  public void testMostProbableNextWord() {
+  public void testMostProbableNextBigramWord() {
+
+    IBagOfNGrams bag = bagOfNGrams();
+
+    @Var
+    int b = 0;
+    @Var
+    int d = 0;
+
+    for (int i = 0; i < 100000; i++) {
+
+      String word = bag.mostProbableNextWord("a");
+
+      if (word.equals("b")) {
+        b++;
+      } else if (word.equals("d")) {
+        d++;
+      } else {
+        Preconditions.checkState(false, "Invalid next word : %s", word);
+      }
+    }
+
+    Assert.assertTrue(49000 <= b && b <= 51000);
+    Assert.assertTrue(49000 <= d && d <= 51000);
+
+    @Var
+    int e = 0;
+    d = 0;
+
+    for (int i = 0; i < 100000; i++) {
+
+      String word = bag.mostProbableNextWord("c");
+
+      if (word.equals("d")) {
+        d++;
+      } else if (word.equals("e")) {
+        e++;
+      } else {
+        Preconditions.checkState(false, "Invalid next word : %s", word);
+      }
+    }
+
+    Assert.assertTrue(24000 <= d && d <= 26000);
+    Assert.assertTrue(74000 <= e && e <= 76000);
+  }
+
+  @Test
+  public void testMostProbableNextTrigramWord() {
 
     IBagOfNGrams bag = bagOfNGrams();
 
@@ -195,6 +278,12 @@ public class IBagOfNGramsTest {
   private Multiset<List<String>> ngrams() {
 
     Multiset<List<String>> bag = HashMultiset.create();
+
+    bag.add(Lists.newArrayList("a", "b"));
+    bag.add(Lists.newArrayList("a", "d"));
+    bag.add(Lists.newArrayList("c", "d"));
+    bag.add(Lists.newArrayList("c", "e"), 3);
+    bag.add(Lists.newArrayList("f", "g"));
 
     bag.add(Lists.newArrayList("a", "b", "c"));
     bag.add(Lists.newArrayList("a", "b", "d"));
