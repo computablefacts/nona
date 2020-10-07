47933785366b0a8de8ccb59f832cad5a0ec50e13
Cyrille Savelief <csavelief@mncc.fr>
2020-10-07 18:44:06 +0200

    Remove dead code.

--- src/com/computablefacts/nona/types/Json.java
+++ src/com/computablefacts/nona/types/Json.java
@@ -1,16 +1,12 @@
 package com.computablefacts.nona.types;
 
-import java.io.IOException;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
 
 import javax.validation.constraints.NotNull;
 
 import com.computablefacts.nona.Generated;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectReader;
-import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.computablefacts.nona.helpers.Codecs;
 import com.google.common.base.MoreObjects;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
@@ -18,9 +14,6 @@ import com.google.errorprone.annotations.CheckReturnValue;
 
 @CheckReturnValue
 final public class Json implements Comparable<Json> {
-
-  private static final ObjectMapper mapper_ = new ObjectMapper();
-  private static final ObjectReader reader_ = mapper_.readerFor(Map.class);
 
   private final Map<String, Object>[] jsons_;
 
@@ -33,36 +26,18 @@ final public class Json implements Comparable<Json> {
 
   public static Json create(String json) {
 
-    Map<String, Object>[] array = asJsonArray(json);
+    Map<String, Object>[] array = Codecs.asArray(json);
 
     if (array != null && array.length > 0) {
       return new Json(array);
     }
 
-    Map<String, Object> object = asJsonObject(json);
+    Map<String, Object> object = Codecs.asObject(json);
 
     if (object != null && !object.isEmpty()) {
       return new Json(new Map[] {object});
     }
     return new Json(new Map[] {});
-  }
-
-  private static @NotNull Map<String, Object> asJsonObject(String json) {
-    try {
-      return reader_.readValue(json);
-    } catch (IOException e) {
-      // TODO
-    }
-    return new HashMap<>();
-  }
-
-  private static @NotNull Map<String, Object>[] asJsonArray(String json) {
-    try {
-      return mapper_.readValue(json, TypeFactory.defaultInstance().constructArrayType(Map.class));
-    } catch (IOException e) {
-      // TODO
-    }
-    return new Map[0];
   }
 
   public int nbObjects() {
