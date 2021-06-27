9fb1a2fffa533054fd698ffa79525db1f4aacfbd
Cyrille Savelief <csavelief@mncc.fr>
2021-06-27 23:18:51 +0200

    Backport function IS_NULL_OR_BLANK(x)

---
+++ src/com/computablefacts/nona/functions/booleanlogicoperators/IsNullOrBlank.java
@@ -0,0 +1,32 @@
+package com.computablefacts.nona.functions.booleanlogicoperators;
+
+import java.util.List;
+
+import com.computablefacts.nona.Function;
+import com.computablefacts.nona.eCategory;
+import com.computablefacts.nona.types.BoxedType;
+import com.google.common.base.Preconditions;
+import com.google.errorprone.annotations.CheckReturnValue;
+
+@CheckReturnValue
+public class IsNullOrBlank extends Function {
+
+  public IsNullOrBlank() {
+    super(eCategory.BOOLEAN_LOGIC_OPERATORS, "IS_NULL_OR_BLANK",
+        "IS_NULL_OR_BLANK(x) returns true if x is null or equals to the empty string after trimming.");
+  }
+
+  @Override
+  public BoxedType<?> evaluate(List<BoxedType<?>> parameters) {
+
+    Preconditions.checkArgument(parameters.size() == 1,
+        "IS_NULL_OR_BLANK takes exactly one parameter.");
+
+    if (parameters.get(0).isEmpty()) {
+      return BoxedType.create(true);
+    }
+
+    String string = parameters.get(0).asString();
+    return BoxedType.create(string == null || string.trim().isEmpty());
+  }
+}
