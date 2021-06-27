05d33e5284b78f8807c10cfd51f0b2d2fa145c88
Cyrille Savelief <csavelief@mncc.fr>
2021-06-28 00:24:55 +0200

    Backport test for extraction of a substring of a BigInteger coerced as a string.

--- test/com/computablefacts/nona/functions/stringoperators/SubstringTest.java
+++ test/com/computablefacts/nona/functions/stringoperators/SubstringTest.java
@@ -21,4 +21,11 @@ public class SubstringTest {
     Function fn = new Function("SUBSTRING(" + Function.wrap("Hello world!") + ", 6, 11)");
     Assert.assertEquals(BoxedType.create("world"), fn.evaluate(Function.definitions()));
   }
+
+  @Test
+  public void testExtractSubstringFromBigInteger() {
+
+    Function fn = new Function("SUBSTRING(53064045700012, 0, 9)");
+    Assert.assertEquals(BoxedType.create("530640457"), fn.evaluate(Function.definitions()));
+  }
 }
