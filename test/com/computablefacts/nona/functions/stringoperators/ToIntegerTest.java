d566a2d6f7186d3129aa95eec06d6a6db22d951d
Cyrille Savelief <csavelief@mncc.fr>
2021-10-17 18:48:37 +0200

    Backport missing test (decimal to integer) for function TO_INTEGER

--- test/com/computablefacts/nona/functions/stringoperators/ToIntegerTest.java
+++ test/com/computablefacts/nona/functions/stringoperators/ToIntegerTest.java
@@ -9,9 +9,16 @@ import com.computablefacts.nona.types.BoxedType;
 public class ToIntegerTest {
 
   @Test
-  public void testToInteger() {
+  public void testIntegerToInteger() {
 
     Function fn = new Function("TO_INTEGER(2018.0)");
     Assert.assertEquals(BoxedType.create(2018), fn.evaluate(Function.definitions()));
+  }
+
+  @Test
+  public void testDecimalToInteger() {
+
+    Function fn = new Function("TO_INTEGER(2018.123)");
+    Assert.assertEquals(BoxedType.empty(), fn.evaluate(Function.definitions()));
   }
 }
